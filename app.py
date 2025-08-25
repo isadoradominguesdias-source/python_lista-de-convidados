c9da151089ef63f6cd9000745877c35e27c07ea4
isadoradominguesdias-source <isadora.domingues.dias@escola.pr.gov.br>
2025-08-25 09:41:37 -0300

    criei a lisra

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
 # Lista de convidados que foram convidados para a festa 
+convidados_da_festa = ['Nayelli','Djulia','Gabriel','Bianca','Guilherme','Dniela','Breno','Nathalia']
+
+# dicionario vazio para armazenar o status de presenca de cada pessoa
+status_presenca = {}
+
+lista de chegada = ['Nayelli','Djulia','Bianca','Breno','Guilherme']
